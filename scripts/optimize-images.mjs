d386f30da72a701aa22c0c03a753cac55edf46b8
helena-cruse <helena.m.cruse@gmail.com>
2026-06-06 08:59:32 +0200

    redirect fix for netlify

--- scripts/optimize-images.mjs
+++ scripts/optimize-images.mjs
@@ -4,12 +4,13 @@ const images = [
   "public/images/AuctionHouse.webp",
   "public/images/hashtagSocial.webp",
   "public/images/littleshop.webp",
+  "public/images/hero.webp",
 ];
 
 for (const image of images) {
   await sharp(image)
     .resize({ width: 1200, withoutEnlargement: true })
-    .webp({ quality: 68 })
+    .webp({ quality: 70 })
     .toFile(image.replace(".webp", "-optimized.webp"));
 }
 
